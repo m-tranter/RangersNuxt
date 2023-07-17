b08766d101875a3d11286b6d3d5cd2e0386bdf4b
antiloquax <tranter.m@sky.com>
2023-07-17 17:28:41 +0100

    Removed redirect

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -1,6 +1,3 @@
 export default defineNuxtRouteMiddleware((to) => {
   console.log(to.path);
-  if (to.path.startsWith('/_') || to.path.startsWith('/events')) {
-    return navigateTo('/rangersnuxt' + to.path)
-  }
 })

88afb319652357ed03ebe70cdd2774f3417e57f4
antiloquax <tranter.m@sky.com>
2023-07-18 18:52:44 +0100

    Trying to set buildAssetsDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-  head: {
+    buildAssetsDir: '/rangersevents/',
+    head: {
     title: 'Ranger Events',
     htmlAttrs: {
       lang: 'en'

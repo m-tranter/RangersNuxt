c596c8cb41001da90af7d3fcaa53b1d90a3ee5f6
antiloquax <tranter.m@sky.com>
2023-07-18 19:10:05 +0100

    Change to buildAssetsDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    buildAssetsDir: '/rangersevents/',
+    buildAssetsDir: 'rangersevents/',
     head: {
     title: 'Ranger Events',
     htmlAttrs: {

1d006bfe2c883fe5f14700897bd1b5346040c04b
antiloquax <tranter.m@sky.com>
2023-07-19 11:02:28 +0100

    Putting serve static back

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+    nitro: {
+    serveStatic: true,
+  },
   app: {
     buildAssetsDir: '/scripts/',
     head: {
